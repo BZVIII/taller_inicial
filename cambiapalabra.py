97f16af689b41793f9390b59f1fcf5cde3aa6262
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-07-16 22:37:02 +0200

    por parámetro

--- cambiapalabra.py
+++ cambiapalabra.py
@@ -1,7 +1,11 @@
-original = input("introduce original")
-nueva = input("introduce nueva")
+import sys
 
-nombreF = "fichero.txt"
+argumentos = sys.argv
+
+nombreF = argumentos[1]
+original = argumentos[2]
+nueva = argumentos[3]
+
 
 f = open(nombreF, "r")  
 
