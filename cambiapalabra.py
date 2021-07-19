7e7690805d88778619b29543e2b6e20dbb612832
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-07-19 21:48:57 +0200

    Version final

--- cambiapalabra.py
+++ cambiapalabra.py
@@ -2,6 +2,26 @@ import sys
 
 argumentos = sys.argv
 
+while len(argumentos) < 4:
+    if len(argumentos) < 2:
+        p = input("Nombre de fichero: ")
+        if len(p) == 0:
+            continue
+        argumentos.append(p)
+
+    if len(argumentos) < 3:
+        p = input("palabra original: ")
+        if len(p) == 0:
+            continue
+        argumentos.append(p)
+
+    if len(argumentos) < 4:
+        p = input("nueva palabra: ")
+        if len(p)== 0:
+            continue
+        argumentos.append(p)
+
+
 nombreF = argumentos[1]
 original = argumentos[2]
 nueva = argumentos[3]
---
+++ elprogramadejuanjose.py
@@ -0,0 +1,5 @@
+import sys
+
+print(sys.argv)
+
+print("la palabra despues de este programa es "+  sys.argv[1])
